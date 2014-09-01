32188f3d7746cd990b1986db5fdac9808d0edefc
anushkamahesh <maheshdesilva89@gmail.com>
2014-09-01 12:55:47 +0530

    new classes added

---
+++ src/java/ajaxdemo/ProfileDetailGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajaxdemo;
+
+/**
+ *
+ * @author Anushka
+ */
+public class ProfileDetailGenerator {
+
+    private String imageURL = null;
+    private String name = null;
+    private String title = null;
+    private String projects = null;
+    private String publications = null;
+}
