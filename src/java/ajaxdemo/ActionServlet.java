0f4683fc5dcced474c9dcc593029588a531b9d60
anushkamahesh <maheshdesilva89@gmail.com>
2014-09-01 13:54:06 +0530

    new classes added

--- src/java/ajaxdemo/ActionServlet.java
+++ src/java/ajaxdemo/ActionServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.midi.Soundbank;
 import org.json.simple.JSONObject;
 
 /**
@@ -47,45 +48,59 @@ public class ActionServlet extends HttpServlet {
             throws ServletException, IOException {
         ObjectClass nn = new ObjectClass();
         String imgeJSON = null;
-        String detailJSON = null;
+        String detailJSON = "";
         PrintWriter out = response.getWriter();
-
+        ProfileDetailGenerator profile = new ProfileDetailGenerator();
+        profile.detailFiller();
 
         imgeJSON = "<div class=\"profile\">\n"
-                + "                                <img src=\"http://www.moorestephens.com/Managed/Images/people%20photos/IOM.jpg\" alt=\"profile2\">                                \n"
+                + "                                <img src=\"" + profile.getImageURL() + "\" alt=\"profile\">                                \n"
                 + "                            </div>\n"
-                + "                            <h1><span>Amy White </span><span>Web Developer</span></h1>";
+                + "                            <h1><span>" + profile.getName() + " </span><span>" + profile.getTitle() + "</span></h1>";
         if (request.getParameter("user").toString().equals("")) {
             imgeJSON = "<h1><span>Ha! </span><span>tried a blank search didn't you? try again</span></h1>";
         }
-        detailJSON = "<section>\n"
-                + "                            <h2>Web Development</h2>\n"
-                + "                            <p>Scenario archetype complementary responsive script pixel sidebar sitemap keep it simple. Complementary visuals footer CSS from alan cooper delightful. Photoshop iconography simplicity user experience affordance narrative ascenders contour. Slab serif interstitial skeuomorphism illustrator design by committee simplicity alan cooper eye tracking. Typography contrast mental model typesetting affordance narrative from CSS. Retina simplicity design by committee typography oblique.</p>\n"
-                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
-                + "                        </section>\n"
-                + "                        <section>\n"
-                + "                            <h2>Constraints</h2>\n"
-                + "                            <p>Card-sorting dropdown constraints alan cooper prägnanz iconography. Stakeholder balsamiq italic vector composition ethnography CSS monospace hierarchy. Eye tracking serif persona focus group.</p>\n"
-                + "                            <p>Typesetting artifact rounded corners eye tracking braindump drawer menu mockup dropdown measure. Jony ive eye tracking script monospace heuristic tabs sketches. Affordance eye tracking scenario usable ligature typesetting clarity responsive. Information architecture golden ratio drawer menu header affordance aspect ratio. Balsamiq slab serif IDEO cognitive dissonance kerning pixel pixel-perfect. Pixel rounded corners header workflow portfolio.</p>\n"
-                + "                            <p>Scenario navigation complementary jony ive helvetica archetype interstitial serif front-end. Constraints pencil usability gestalt design language mockup usability testing affordance jakob nielsen.</p>\n"
-                + "                        </section>\n"
-                + "                        <section>\n"
-                + "                            <h2>Ligature</h2>\n"
-                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
-                + "                            <p>Paper prototype urbanized headroom typography splash screen pencil modal branding. Retina omnigraffle objectified descender navigation adobe ethnography. Innovate design by committee modern hero message. Contrast user-centered color theory keep it simple visuals bevel adobe descender splash screen. From focus group accessibility sans-serif archetype pixel-perfect complementary skeuomorphism. Focus group iconography figure-ground navigation user-centered omnigraffle from.</p>\n"
-                + "                        </section>\n"
-                + "                        <section>\n"
-                + "                            <h2>Ligature</h2>\n"
-                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
-                + "                            <p>Paper prototype urbanized headroom typography splash screen pencil modal branding. Retina omnigraffle objectified descender navigation adobe ethnography. Innovate design by committee modern hero message. Contrast user-centered color theory keep it simple visuals bevel adobe descender splash screen. From focus group accessibility sans-serif archetype pixel-perfect complementary skeuomorphism. Focus group iconography figure-ground navigation user-centered omnigraffle from.</p>\n"
-                + "                        </section>\n"
-                + "                        <section>\n"
-                + "                            <h2>Typesetting</h2>\n"
-                + "                            <p>Typesetting artifact rounded corners eye tracking braindump drawer menu mockup dropdown measure. Jony ive eye tracking script monospace heuristic tabs sketches. Affordance eye tracking scenario usable ligature typesetting clarity responsive. Information architecture golden ratio drawer menu header affordance aspect ratio. Balsamiq slab serif IDEO cognitive dissonance kerning pixel pixel-perfect. Pixel rounded corners header workflow portfolio.</p>\n"
-                + "                            <p>Fireworks mobile skeuomorphism sitemap. Workflow iconography interaction design pixel-perfect serif. Mental model monospace typeface behavior change bauhaus from usability testing. Color theory user experience paper prototype narrative palette serif gradient header. Oblique modal 66-character line sketch responsive portfolio. Comic sans fireworks prägnanz monospace gradient design language jakob nielsen. Figure-ground pixel aspect ratio sketches rounded corners jony ive constraints mental model splash screen.</p>\n"
-                + "                            <p>Placeholder text by <a href=\"http://www.designeripsum.com/\">Designer Ipsum</a>.</p>\n"
-                + "                            <p>Profile images are licensed under a <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.0/deed.en\">Creative Commons BY-NC-SA 2.0</a> license. The images are from Greg Peverill-Conti's <a href=\"http://www.flickr.com/photos/gregpc/\">1,000 faces project</a>.</p>\n"
-                + "                        </section>";
+
+        if (profile.getPersonalDetails() != null) {
+            detailJSON = "<section>\n"
+                    + "<h2>Personal Details</h2>" + profile.getPersonalDetails() + "</section>";
+        }
+        
+        if (profile.getProjects() != null) {
+            detailJSON = detailJSON+"<section>\n"
+                    + "<h2>Project Details</h2>" + profile.getProjects() + "</section>";
+        }
+        if (profile.getPublications() != null) {
+            detailJSON = detailJSON+"<section>\n"
+                    + "<h2>Publication Details</h2>" + profile.getPublications() + "</section>";
+        }
+//                + "                            <h2>Web Development</h2>\n"
+//                + "                            <p>Scenario archetype complementary responsive script pixel sidebar sitemap keep it simple. Complementary visuals footer CSS from alan cooper delightful. Photoshop iconography simplicity user experience affordance narrative ascenders contour. Slab serif interstitial skeuomorphism illustrator design by committee simplicity alan cooper eye tracking. Typography contrast mental model typesetting affordance narrative from CSS. Retina simplicity design by committee typography oblique.</p>\n"
+//                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
+//                + "                        </section>\n"
+//                + "                        <section>\n"
+//                + "                            <h2>Constraints</h2>\n"
+//                + "                            <p>Card-sorting dropdown constraints alan cooper prägnanz iconography. Stakeholder balsamiq italic vector composition ethnography CSS monospace hierarchy. Eye tracking serif persona focus group.</p>\n"
+//                + "                            <p>Typesetting artifact rounded corners eye tracking braindump drawer menu mockup dropdown measure. Jony ive eye tracking script monospace heuristic tabs sketches. Affordance eye tracking scenario usable ligature typesetting clarity responsive. Information architecture golden ratio drawer menu header affordance aspect ratio. Balsamiq slab serif IDEO cognitive dissonance kerning pixel pixel-perfect. Pixel rounded corners header workflow portfolio.</p>\n"
+//                + "                            <p>Scenario navigation complementary jony ive helvetica archetype interstitial serif front-end. Constraints pencil usability gestalt design language mockup usability testing affordance jakob nielsen.</p>\n"
+//                + "                        </section>\n"
+//                + "                        <section>\n"
+//                + "                            <h2>Ligature</h2>\n"
+//                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
+//                + "                            <p>Paper prototype urbanized headroom typography splash screen pencil modal branding. Retina omnigraffle objectified descender navigation adobe ethnography. Innovate design by committee modern hero message. Contrast user-centered color theory keep it simple visuals bevel adobe descender splash screen. From focus group accessibility sans-serif archetype pixel-perfect complementary skeuomorphism. Focus group iconography figure-ground navigation user-centered omnigraffle from.</p>\n"
+//                + "                        </section>\n"
+//                + "                        <section>\n"
+//                + "                            <h2>Ligature</h2>\n"
+//                + "                            <p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n"
+//                + "                            <p>Paper prototype urbanized headroom typography splash screen pencil modal branding. Retina omnigraffle objectified descender navigation adobe ethnography. Innovate design by committee modern hero message. Contrast user-centered color theory keep it simple visuals bevel adobe descender splash screen. From focus group accessibility sans-serif archetype pixel-perfect complementary skeuomorphism. Focus group iconography figure-ground navigation user-centered omnigraffle from.</p>\n"
+//                + "                        </section>\n"
+//                + "                        <section>\n"
+//                + "                            <h2>Typesetting</h2>\n"
+//                + "                            <p>Typesetting artifact rounded corners eye tracking braindump drawer menu mockup dropdown measure. Jony ive eye tracking script monospace heuristic tabs sketches. Affordance eye tracking scenario usable ligature typesetting clarity responsive. Information architecture golden ratio drawer menu header affordance aspect ratio. Balsamiq slab serif IDEO cognitive dissonance kerning pixel pixel-perfect. Pixel rounded corners header workflow portfolio.</p>\n"
+//                + "                            <p>Fireworks mobile skeuomorphism sitemap. Workflow iconography interaction design pixel-perfect serif. Mental model monospace typeface behavior change bauhaus from usability testing. Color theory user experience paper prototype narrative palette serif gradient header. Oblique modal 66-character line sketch responsive portfolio. Comic sans fireworks prägnanz monospace gradient design language jakob nielsen. Figure-ground pixel aspect ratio sketches rounded corners jony ive constraints mental model splash screen.</p>\n"
+//                + "                            <p>Placeholder text by <a href=\"http://www.designeripsum.com/\">Designer Ipsum</a>.</p>\n"
+//                + "                            <p>Profile images are licensed under a <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.0/deed.en\">Creative Commons BY-NC-SA 2.0</a> license. The images are from Greg Peverill-Conti's <a href=\"http://www.flickr.com/photos/gregpc/\">1,000 faces project</a>.</p>\n"
+//                + "                        </section>";
         response.setContentType("application/json");
         try {
             JSONObject result = new JSONObject();
--- src/java/ajaxdemo/ProfileDetailGenerator.java
+++ src/java/ajaxdemo/ProfileDetailGenerator.java
@@ -15,4 +15,40 @@ public class ProfileDetailGenerator {
     private String title = null;
     private String projects = null;
     private String publications = null;
+    private String personalDetails = null;
+
+    public void detailFiller() {
+        imageURL = "http://4.bp.blogspot.com/-PyLH6GtNHBQ/UH5HkcNQwXI/AAAAAAAAAAU/aur0JqaeAZg/s1600/Cool+short+hairstyles+For+Men+1.jpg";
+        name = "Amy Weber";
+        title = "Web Desinger";
+        projects = "<p>Scenario archetype complementary responsive script pixel sidebar sitemap keep it simple. Complementary visuals footer CSS from alan cooper delightful. Photoshop iconography simplicity user experience affordance narrative ascenders contour. Slab serif interstitial skeuomorphism illustrator design by committee simplicity alan cooper eye tracking. Typography contrast mental model typesetting affordance narrative from CSS. Retina simplicity design by committee typography oblique.</p>\n"
+                + "<p>Delightful ascenders contrast prototype. Ligature jakob nielsen user measure. Ligature contrast glyph rule of thirds composition interstitial dribbble. Mental model typography urbanized balance. Resolution rounded corners IDEO constraints dribbble persona. Portfolio sketch baseline 66-character line. Usability testing mental model simplicity aspect ratio pencil type. Usable sans-serif visualization leading prägnanz baseline pencil fireworks clarity omnigraffle.</p>\n";
+        publications = "<p>Card-sorting dropdown constraints alan cooper prägnanz iconography. Stakeholder balsamiq italic vector composition ethnography CSS monospace hierarchy. Eye tracking serif persona focus group.</p>\n"
+                + "<p>Typesetting artifact rounded corners eye tracking braindump drawer menu mockup dropdown measure. Jony ive eye tracking script monospace heuristic tabs sketches. Affordance eye tracking scenario usable ligature typesetting clarity responsive. Information architecture golden ratio drawer menu header affordance aspect ratio. Balsamiq slab serif IDEO cognitive dissonance kerning pixel pixel-perfect. Pixel rounded corners header workflow portfolio.</p>\n"
+                + "<p>Scenario navigation complementary jony ive helvetica archetype interstitial serif front-end. Constraints pencil usability gestalt design language mockup usability testing affordance jakob nielsen.</p>\n";
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getProjects() {
+        return projects;
+    }
+
+    public String getPublications() {
+        return publications;
+    }
+
+    public String getPersonalDetails() {
+        return personalDetails;
+    }
 }
